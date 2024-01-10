9522f0d050cb457ec78b1d93912a743012ceffe2
IvanovKS <Skitchi@mail.ru>
2024-01-10 22:10:03 +0300

    feat: add Todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Todo } from './Todo';
 
 function App() {
+  let tasks1 = [
+    {id: 1, title: 'Магазин', isDone: true},
+    {id: 2, title: 'Машина', isDone: true},
+    {id: 3, title: 'Детский сад', isDone: false},
+  ];
+  let tasks2 = [
+    {id: 1, title: 'Больница', isDone: true},
+    {id: 2, title: 'Бассейн', isDone: false},
+    {id: 3, title: 'Парк', isDone: false},
+  ];
   return (
     <div className="App">
-      todo_ts
+      <Todo title = "Monday" tasks = {tasks1}/>
+      <Todo title = "Tuesday" tasks = {tasks2}/>
+      {/* <Todo title = "Wednesday"/> */}
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,41 @@
+import { title } from 'process';
+import React from 'react';
+
+type TaskType = {
+  id: number,
+  isDone: boolean,
+  title: string,
+}
+
+type PropsType = {
+  title: string,
+  tasks: Array<TaskType>,
+}
+
+export function Todo(props: PropsType) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type="checkbox" checked={props.tasks[0].isDone} /><span>{props.tasks[0].title}</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={props.tasks[1].isDone} /><span>{props.tasks[1].title}</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={props.tasks[2].isDone} /><span>{props.tasks[2].title}</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+}
