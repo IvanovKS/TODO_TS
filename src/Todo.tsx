29df8e371ac13152e703344214b667459518be5e
IvanovKS <Skitchi@mail.ru>
2024-01-12 20:21:03 +0300

    feat: add css classes for buttons, add error for empty field

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,7 @@
 // import { title } from 'process';
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { FilterValuesType } from './App';
+import { error } from 'console';
 
 export type TaskType = {
   id: string,
@@ -15,22 +16,28 @@ type PropsType = {
   changeFilter: (value: FilterValuesType) => void,
   addTask: (title: string) => void,
   changeTaskStatus: (taskId: string, isDone: boolean) => void,
+  filter: FilterValuesType,
 }
 
 export function Todo(props: PropsType) {
   const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   // ***functions
-  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setNewTaskTitle(event.currentTarget.value);
-  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      props.addTask(newTaskTitle);
-      setNewTaskTitle("");
-    }
-  };
   const addTask = () => {
+    if (newTaskTitle.trim() === "") {
+      setError("Обязательное поле");
+      return;
+    }
     props.addTask(newTaskTitle);
     setNewTaskTitle("");
+  };
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => setNewTaskTitle(event.currentTarget.value);
+  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    setError(null);
+    if (event.key === "Enter") {
+      addTask();
+    }
   };
   const onAllClickHandler = () => {props.changeFilter("all")};
   const onActiveClickHandler = () => {props.changeFilter("active")};
@@ -44,15 +51,17 @@ export function Todo(props: PropsType) {
         <input  value={newTaskTitle}
                 onChange={onChangeHandler}
                 onKeyDown={onKeyDownHandler}
+                className={error ? "error" : ""}
                 />
         <button onClick={addTask}>+</button>
+        {error && <div className="error-message">{error}</div>}
       </div>
       <ul>
         {
           props.tasks.map((el) => {
             const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {props.changeTaskStatus(el.id, event.currentTarget.checked)};
             return (
-              <li key={el.id}>
+              <li key={el.id} className={el.isDone ? "is-done" : ""}>
                 <input  type="checkbox" 
                         checked={el.isDone}
                         onChange={onChangeHandler}
@@ -67,9 +76,12 @@ export function Todo(props: PropsType) {
         }
       </ul>
       <div>
-        <button onClick={onAllClickHandler}>All</button>
-        <button onClick={onActiveClickHandler}>Active</button>
-        <button onClick={onCompletedClickHandler}>Completed</button>
+        <button className={props.filter === "all" ? "active-filter" : ""}
+                onClick={onAllClickHandler}>All</button>
+        <button className={props.filter === "active" ? "active-filter" : ""}
+                onClick={onActiveClickHandler}>Active</button>
+        <button className={props.filter === "completed" ? "active-filter" : ""}
+                onClick={onCompletedClickHandler}>Completed</button>
       </div>
     </div>
   );
